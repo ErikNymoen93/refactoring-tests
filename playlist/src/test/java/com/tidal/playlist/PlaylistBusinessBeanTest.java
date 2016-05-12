be771b10c760cb8dd14343c9f18df568720846c0
ErikNymoen93 <erik.nymoen93@gmail.com>
2016-05-12 23:09:06 +0200

    Update PlaylistBusinessBeanTest.java
    Added a new track, added a new unit test to see if the new track was added, added a unit test to check if the delete track method was working.

--- playlist/src/test/java/com/tidal/playlist/PlaylistBusinessBeanTest.java
+++ playlist/src/test/java/com/tidal/playlist/PlaylistBusinessBeanTest.java
@@ -3,6 +3,7 @@ package com.tidal.playlist;
 import com.google.inject.Inject;
 import com.tidal.playlist.data.PlayListTrack;
 import com.tidal.playlist.data.Track;
+import com.tidal.playlist.dao.PlaylistDaoBean;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Guice;
@@ -22,6 +23,13 @@ import static org.testng.AssertJUnit.assertTrue;
  */
 @Guice(modules = TestBusinessModule.class)
 public class PlaylistBusinessBeanTest {
+	
+	private PlaylistDaoBean playlistDaoBean;
+
+	@Inject
+	    public PlaylistBusinessBeanTest(PlaylistDaoBean playlistDaoBean){
+	        this.playlistDaoBean = playlistDaoBean;
+	    }
 
     @Inject
     PlaylistBusinessBean playlistBusinessBean;
@@ -37,7 +45,7 @@ public class PlaylistBusinessBeanTest {
     }
 
     @Test
-    public void testAddTracks() throws Exception {
+    public void testAddAndRemoveTracks() throws Exception {
         List<Track> trackList = new ArrayList<Track>();
 
         Track track = new Track();
@@ -47,9 +55,38 @@ public class PlaylistBusinessBeanTest {
         track.setId(76868);
 
         trackList.add(track);
+        
+        //Adding a new track
+        track = new Track();
+        track.setArtistId(6);
+        track.setTitle("Another brand new track");
+        track.setTrackNumberIdx(2);
+        track.setId(15371);
 
-        List<PlayListTrack> playListTracks = playlistBusinessBean.addTracks(UUID.randomUUID().toString(), 1, trackList, 5, new Date());
+        trackList.add(track);
+        
+        //Creating uuid string
+        String uuid = UUID.randomUUID().toString();
 
-        assertTrue(playListTracks.size() > 0);
+        List<PlayListTrack> playListTracks = playlistBusinessBean.addTracks(uuid, 1, trackList, 5, new Date());
+
+        
+        assertTrue(playListTracks.size() > 0);  //Checks if there are any tracks in the list
+        assertTrue(playListTracks.size() == 2); //Checks if there are 2 tracks in the list (too see if the new one is added)
+        
+        TrackPlayList playList = playlistDaoBean.getPlaylistByUUID(uuid, 1);
+        
+        List<PlayListTrack> tracksToDelete = new ArrayList<PlayListTrack>();
+        
+        //Deleting track by it's Id (15371)
+        tracksToDelete.add(playList.getPlayListTrack(15371));
+        
+        playlistBusinessBean.deleteTracks(uuid, 1, tracksToDelete);
+        
+        assertTrue(playList.getPlayListTracksSize() < 2); //checks if there are less than two tracks in the list, which it should be if the track was deleted
+        
+
+        
     }
+    
 }
